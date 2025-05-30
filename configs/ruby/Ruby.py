1ec8f0c6c7b474430e89fe3ea93df55b1a316e76
erwq <123213112a@gmail.com>
2025-05-30 16:19:05 +0800

    configs: Use importlib for robust dynamic loading in Ruby.py

--- configs/ruby/Ruby.py
+++ configs/ruby/Ruby.py
@@ -37,8 +37,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import importlib
 import math
-from importlib import import_module
 
 import m5
 from m5.defines import buildEnv
@@ -126,8 +126,11 @@ def define_options(parser):
         help="Recycle latency for ruby controller input buffers",
     )
 
-    import_module(f"ruby.{buildEnv['PROTOCOL']}").define_options(parser)
-
+    protocol = buildEnv["PROTOCOL"]
+    protocol_module = importlib.import_module(
+        f".{protocol}", package=__package__
+    )
+    protocol_module.define_options(parser)
     Network.define_options(parser)
 
 
@@ -213,8 +216,11 @@ def create_topology(controllers, options):
     found in configs/topologies/BaseTopology.py
     This is a wrapper for the legacy topologies.
     """
-    exec(f"import topologies.{options.topology} as Topo")
-    topology = eval(f"Topo.{options.topology}(controllers)")
+    topology_class = getattr(
+        importlib.import_module(f"topologies.{options.topology}"),
+        options.topology,
+    )
+    topology = topology_class(controllers=controllers)
     return topology
 
 
@@ -246,19 +252,20 @@ def create_system(
     if cpus is None:
         cpus = system.cpu
 
+    protocol_name_str = buildEnv["PROTOCOL"]
     try:
-        (cpu_sequencers, dir_cntrls, topology) = import_module(
-            f"ruby.{buildEnv['PROTOCOL']}"
-        ).create_system(
+        protocol_module = importlib.import_module(
+            f".{protocol_name_str}", package=__package__
+        )
+        create_system_fn = getattr(protocol_module, "create_system")
+        (cpu_sequencers, dir_cntrls, topology) = create_system_fn(
             options, full_system, system, dma_ports, bootmem, ruby, cpus
         )
     except:
         print(
-            "Error: could not create sytem for ruby protocol "
-            f"{buildEnv['PROTOCOL']}"
+            f"Error: could not create sytem for ruby protocol {protocol_name_str}"
         )
         raise
-
     # Create the network topology
     topology.makeTopology(
         options, network, IntLinkClass, ExtLinkClass, RouterClass
@@ -305,25 +312,6 @@ def create_system(
 
 
 def create_directories(options, bootmem, ruby_system, system):
-    import importlib
-
-    try:
-        # The supported way to use Ruby is now to use the protocol name as
-        # part of the names for all of the controllers. This is *required*
-        # when using `MULTIPLE` as the protocol and the `ALL` target.
-        Directory_Controller = getattr(
-            importlib.import_module("m5.objects"),
-            f"{options.protocol}_Directory_Controller",
-        )
-    except AttributeError:
-        # This is a fallback for the legacy Ruby protocols. If you can't
-        # find the protocol-specific directory controller, then use the
-        # generic one. This is a hack that only works if you have a single
-        # protocol.
-        Directory_Controller = getattr(
-            importlib.import_module("m5.objects"), "Directory_Controller"
-        )
-
     dir_cntrl_nodes = []
     for i in range(options.num_dirs):
         dir_cntrl = Directory_Controller()
