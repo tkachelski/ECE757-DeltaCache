8981cdb45378a8bb7f88b3efb88f21d789d12c7d
David Schall <david.schall@tum.de>
2025-06-22 15:43:20 +0000

    cpu: Add FDP support for TAGE-SC-L.
    In order to work with FDP TAGE-SC-L must implement
    the branchPlaceholder function.

--- src/cpu/pred/bpred_unit.cc
+++ src/cpu/pred/bpred_unit.cc
@@ -408,6 +408,11 @@ BPredUnit::squash(const InstSeqNum &squashed_sn, ThreadID tid)
 
         auto hist = predHist[tid].front();
 
+        DPRINTF(Branch,
+                "[tid:%i, squash sn:%llu] Removing history for "
+                "sn:%llu, PC:%#x\n",
+                tid, squashed_sn, hist->seqNum, hist->pc);
+
         squashHistory(tid, hist);
 
         predHist[tid].pop_front();
