3fec320a14046959d110f9a061a4b02c1b7658d0
Yu-hsin Wang <yuhsingw@google.com>
2025-07-29 11:39:36 +0800

    systemc: Fix the potential initialization order issue
    The underlying data of sc_object is stored in _gem5_object. It should be
    assigned before child object construction. Otherwise it would crash when
    child accessing the parent names during the construction.
    
    Test: ./src/systemc/tests/verify.py \
            --update-json build/NULL -j `nproc` \
            --filter-file ./src/systemc/tests/working.filt
    
    Change-Id: I59e1d04963b047c1ce70623df8d360d93ae9f8e6

--- src/systemc/core/sc_object.cc
+++ src/systemc/core/sc_object.cc
@@ -123,19 +123,18 @@ sc_object::simcontext() const
     return _gem5_object->simcontext();
 }
 
-sc_object::sc_object()
+sc_object::sc_object() : _gem5_object(new sc_gem5::Object(this))
 {
-    _gem5_object = new sc_gem5::Object(this);
 }
 
 sc_object::sc_object(const char *name)
+  : _gem5_object(new sc_gem5::Object(this, name))
 {
-    _gem5_object = new sc_gem5::Object(this, name);
 }
 
 sc_object::sc_object(const sc_object &other)
+  : _gem5_object(new sc_gem5::Object(this, *other._gem5_object))
 {
-    _gem5_object = new sc_gem5::Object(this, *other._gem5_object);
 }
 
 sc_object &
